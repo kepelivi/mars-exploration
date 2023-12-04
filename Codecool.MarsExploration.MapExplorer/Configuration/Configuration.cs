f4d2d1c3e4d6025749680d3679c47b7dbe22a770
Kbence9 <bence.kristian@gmail.com>
2023-12-04 11:08:02 +0100

    Configuration record added and configuration validator implemented, map loader need for one more method

---
+++ Codecool.MarsExploration.MapExplorer/Configuration/Configuration.cs
@@ -0,0 +1,5 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Configuration;
+
+public record Configuration(string MapFile, Coordinate LandingCoordinate, IEnumerable<string> Resources, int MaxSteps);
