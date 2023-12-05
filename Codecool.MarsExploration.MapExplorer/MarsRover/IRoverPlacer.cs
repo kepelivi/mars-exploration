6bad9b3e77d6c8eb8d25462025885a292eff5b95
kepelivi <kepelivi7@gmail.com>
2023-12-05 09:43:25 +0100

    feat - added new feature RoverPlace interface

---
+++ Codecool.MarsExploration.MapExplorer/MarsRover/IRoverPlacer.cs
@@ -0,0 +1,15 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecool.MarsExploration.MapExplorer.MarsRover
+{
+    public interface IRoverPlacer
+    {
+        MarsRover PlaceRover(string id, Map map, Coordinate shipPosition, int sight);
+    }
+}
